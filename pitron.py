b06040616769b76987cdd7a85a51f5a7be1972ce
idifahs <357awc@gmail.com>
2017-05-12 00:28:40 -0500

    Add files via upload

--- pitron.py
+++ pitron.py
@@ -1,253 +1,315 @@
-# PiTron v0.0.1a
-# Snake controlled with two GPIO switches
-from random import randint
-from pygame.locals import *
 import RPi.GPIO as GPIO
+from pygame.locals import *
+from random import randint
+from grid import Grid
+from powerups import *
+from snake import Snake
+
 import pygame
 import sys
-import time
 
 # Window dimensions
-WINDOW_W = 640
-WINDOW_H = 480
+WINDOW_W = 1024
+WINDOW_H = 768
 
-# Movement step
-# Coordinates are scaled by this
-GRIDSIZE = 32
+# Grid dimensions
+CELL_SIZE = 8
+GRID_W = 75
+GRID_H = 75
 
-# Number of grid cells in window
-GRID_W = WINDOW_W / GRIDSIZE
-GRID_H = WINDOW_H / GRIDSIZE
+GRID_OFFSET_X = (WINDOW_W - GRID_W * CELL_SIZE) / 2
+GRID_OFFSET_Y = (WINDOW_H - GRID_H * CELL_SIZE) / 2
 
-# GPIO keys
-KLEFT = 26
-KRIGHT = 21
+BACKGROUND = "space.png"
 
-# Info strings
-VERSION = 'PiTron v0.0.1a'
-IMAGEDR = 'img/'
+# Player start positions
+POS_1 = (GRID_W / 2, 0) # Top
+POS_2 = (GRID_W - 1, GRID_H / 2) # Right
+POS_3 = (GRID_W / 2, GRID_H - 1) # Bottom
+POS_4 = (0, GRID_H / 2) # Left
 
-# Returns true if the coordinates match up
-# i.e, they're on the same grid spot
-def sameGrid(x1, y1, x2, y2):
-	return x1 == x2 and y1 == y2
+# GPIO input pins
+PINS = [4, 17, 27, 22, 5, 6, 13, 19]
 
-# Increases length of player when 'eaten'
-class Apple(object):
-	def __init__(self):
-		self.x = 0
-		self.y = 0
-		self.reposition()
-
-		self.img = pygame.image.load(IMAGEDR + "apple.png").convert()
-
-	# Repositions apple to a random spot on the grid
-	def reposition(self):
-		self.x = randint(1, GRID_W - 1) * GRIDSIZE
-		self.y = randint(1, GRID_H - 1) * GRIDSIZE
-
-	def draw(self, surface):
-		surface.blit(self.img, (self.x, self.y))
-
-# Player class
-class Player(object):
-	# Direction constants
-	LEFT, RIGHT, UP, DOWN = range(4)
-
-	def __init__(self):
-		# Direction stuff
-		self.direction = Player.RIGHT
-		self.changeDir = None
-		
-		# Setup start length
-		self.length = 3
-		self.x = [0, 1, 2]
-		self.y = [0, 0, 0]
-
-		# Counters used to step updates
-		self.updateDelay = 45
-		self.updateCount = 0
-
-		self.img = pygame.image.load(IMAGEDR + "snake.png").convert()
-
-	# main update method
-	def update(self):
-		self.updateCount += 1
-
-		if self.updateCount > self.updateDelay:
-			# Change direction if needed
-			if self.changeDir != None:
-				self.direction = self.changeDir
-				self.changeDir = None
-
-			# Update previous positions
-			for i in range(self.length - 1, 0, -1):
-				self.x[i] = self.x[i - 1]
-				self.y[i] = self.y[i - 1]
-
-			# Update current position
-			if self.direction == Player.LEFT:
-				self.x[0] -= GRIDSIZE
-			elif self.direction == Player.RIGHT:
-				self.x[0] += GRIDSIZE
-			elif self.direction == Player.UP:
-				self.y[0] -= GRIDSIZE
-			elif self.direction == Player.DOWN:
-				self.y[0] += GRIDSIZE
-
-			# Reset delay count
-			self.updateCount = 0
-
-	# Add another tail block
-	def increase(self):
-		self.x.append(self.x[self.length - 1])
-		self.y.append(self.y[self.length - 1])
-		self.length += 1
-
-	# Direction setters 
-	def moveLeft(self):
-		if self.direction != Player.RIGHT:
-			self.changeDir = Player.LEFT
-
-	def moveRight(self):
-		if self.direction != Player.LEFT:
-			self.changeDir = Player.RIGHT
-
-	def moveUp(self):
-		if self.direction != Player.DOWN:
-			self.changeDir = Player.UP
-
-	def moveDown(self):
-		if self.direction != Player.UP:
-			self.changeDir = Player.DOWN
-
-	def draw(self, surface):
-		for i in range(0, self.length):
-			surface.blit(self.img, (self.x[i], self.y[i]))
-
-# Main game class
 class Game(object):
 	def __init__(self):
+		# Set up display
+		flags = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN
+
+		if 'fullscreen' in sys.argv:
+			flags = flags | pygame.FULLSCREEN
+
+		self.display = pygame.display.set_mode((WINDOW_W,WINDOW_H), flags)
+		self.display.set_alpha(None)
+		pygame.display.set_caption('PiTron')
+
+		# Main grid
+		self.board = Grid(GRID_W, GRID_H, CELL_SIZE, GRID_OFFSET_X, GRID_OFFSET_Y)
+
+		# Game lists
+		self.powerupclasses = []
+		self.powerups = []
+		self.players = []
+
+		# Game timer
+		self.clock = pygame.time.Clock()
+
+		# Key inputs
+		# Key = player index
+		# Val = [left button, right button, left pressed, right pressed]
+		self.inputs = { 0 : [pygame.K_a, pygame.K_d, False, False], \
+						1 : [pygame.K_LEFT, pygame.K_RIGHT, False, False], \
+						2 : [pygame.K_f, pygame.K_h, False, False], \
+						3 : [pygame.K_j, pygame.K_l, False, False]}
+
+		self.inputs = {}
+
+		# Text rendering
+		self.font = pygame.font.SysFont("monospace", 15)
+		
+		# Background image
+		self.background = pygame.image.load(BACKGROUND).convert()
+		self.background_rect = self.background.get_rect()
+
+		# Board rotation
+		self.boardangle = 0.0
+		self.rotationtimer = 7500
+		self.rotating = False
+		
+		# Input flipping
+		self.fliptimer = 5000
+		self.flipped = False
+
+		# Render blanking
+		self.blanktimer = 15000
+		self.blankdelay = 2000
+		self.renderblanked = False
+
+		# Powerup spawning
+		self.spawnfreq = 20
+
+		# Gamestate
 		self.running = False
-		self.display = None
-		self.player = None
-		self.apple = None
+		self.oneleft = False
 
-		# Keep track of GPIO presses
-		self.lpressed = False
-		self.rpressed = False
+	def start(self):
+		self.powerupclasses.append(GhostPU)
+		self.powerupclasses.append(SpeedPU)
+		self.powerupclasses.append(PortalPU)
+		self.powerupclasses.append(WidthPU)
 
-	def on_init(self):
-		pygame.init()
-		self.display = pygame.display.set_mode((WINDOW_W, WINDOW_H), \
-							pygame.HWSURFACE)
-		pygame.display.set_caption(VERSION)
+		self.players.append(Snake(POS_1[0], POS_1[1], self.board, Snake.DOWN))
+		self.players.append(Snake(POS_3[0], POS_3[1], self.board, Snake.UP, [0,0,255]))
 
-		self.player = Player()
-		self.apple = Apple()
+		self.inputs[0] = [PINS[0], PINS[1], False, False]
+		self.inputs[1] = [PINS[2], PINS[3], False, False]
+
+		if '4p' in sys.argv:
+			self.players.append(Snake(POS_2[0], POS_2[1], self.board, Snake.LEFT, [225, 0, 225]))
+			self.players.append(Snake(POS_4[0], POS_4[1], self.board, Snake.RIGHT, [255,255,0]))
+
+			self.inputs[2] = [PINS[4], PINS[5], False, False]
+			self.inputs[3] = [PINS[6], PINS[7], False, False]
 
 		self.running = True
 
-	def on_event(self, event):
-		if event.type == QUIT:
-			self.running = False
+		#print self.board
+		self.loop()
 
-	def on_loop(self):
-		self.player.update()
+	def loop(self):
+		while self.running:
+			# Get time between frames
+			delta = self.clock.tick()
 
-		# Check player-apple collision
-		for i in range(self.player.length):
-			if sameGrid(self.apple.x, self.apple.y, \
-					self.player.x[i], self.player.y[i]):
-				self.apple.reposition()
-				self.player.increase()
+			# Handle board rotation
+			if not self.rotating:
+				self.rotationtimer -= delta
 
-		# Check player collision with self
-		for i in range(2, self.player.length):
-			if sameGrid(self.player.x[0], self.player.y[0], \
-					self.player.x[i], self.player.y[i]):
-				print "You lose"
-				self.on_cleanup()
+				if self.rotationtimer <= 0:
+					self.rotationtimer = 7500
+					self.rotating = True
 
-		# Check if player is offscreen
-		if self.player.x[0] < 0 or self.player.x[0] > GRID_W * GRIDSIZE or \
-			self.player.y[0] < 0 or self.player.y[0] > GRID_H * GRIDSIZE:
-			print "You lose"
-			self.on_cleanup()
+			# Handle input flipping
+			self.fliptimer -= delta
 
-	def on_render(self):
+			if self.fliptimer <= 0:
+				self.fliptimer = 5000
+				self.flipped = not self.flipped
+
+			# Handle render pausing
+			if not self.renderblanked:
+				self.blanktimer -= delta
+
+				if self.blanktimer <= 0:
+					self.blanktimer = 15000
+					self.renderblanked = True
+			else:
+				self.blankdelay -= delta
+				
+				if self.blankdelay <= 0:
+					self.blankdelay = 2000
+					self.renderblanked = False
+
+			# Spawn powerups
+			spawn = randint(1, 1000)
+
+			if spawn >= 1000 - self.spawnfreq:
+				ptype = randint(0, len(self.powerupclasses) - 1)
+
+				self.powerups.append(self.powerupclasses[ptype](self.board))
+
+			# Handle 'x' being clicked
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.cleanup()
+			
+			# Handle input
+			keys = pygame.key.get_pressed()
+
+			for key in self.inputs.keys():
+				value = self.inputs[key]
+
+				left = value[0]
+				right = value[1]
+				lpressed = value[2]
+				rpressed = value[3]
+				
+				
+				if GPIO.input(left):
+					if not lpressed:
+						self.inputs[key][2] = True
+						self.players[key].changeDirection(-1 if not self.flipped else 1)
+				else:
+					self.inputs[key][2] = False
+					
+				if GPIO.input(right):
+					if not rpressed:
+						self.inputs[key][3] = True
+						self.players[key].changeDirection(1 if not self.flipped else -1)
+				else:
+					self.inputs[key][3] = False
+				
+				
+			if keys[K_ESCAPE]:
+				self.running = False
+
+			# Update and render everything
+			self.update(delta)
+			self.draw()
+
+		# Quit
+		self.cleanup()
+
+	def update(self, delta):
+		for snake in self.players:
+			snake.update(delta)
+
+		for powerup in self.powerups:
+			powerup.update(delta)
+
+			if powerup.used:
+				self.powerups.remove(powerup)
+				self.board.setCell(None, powerup.x, powerup.y)
+
+	def draw(self):
+		# Background
 		self.display.fill((0,0,0))
-		self.player.draw(self.display)
-		self.apple.draw(self.display)
+		self.display.blit(self.background, self.background_rect)
+
+		# Draw everything to grid
+		boardsurf = self.board.render(self.display, hide=self.renderblanked)
+		boardrect = boardsurf.get_rect()
+
+		boardrect.topleft = (GRID_OFFSET_X, GRID_OFFSET_Y)
+
+		# Increase board rotation
+		if self.rotating:
+			self.boardangle += 0.5
+
+			if self.boardangle % 90.0 == 0.0:
+				self.rotating = False
+
+			if self.boardangle >= 360:
+				self.boardangle = 0
+
+		# Invert colors when flipped
+		if self.flipped:
+			inv = pygame.Surface(boardrect.size, pygame.SRCALPHA)
+			inv.fill((255,255,255,255))
+			inv.blit(boardsurf, (0,0), None, pygame.BLEND_RGB_SUB)
+			boardsurf = inv
+
+		# Dark orange filter when blanked
+		if self.renderblanked:
+			orn = pygame.Surface(boardrect.size, pygame.SRCALPHA)
+			orn.fill((255,175,0,128))
+			orn.blit(boardsurf, (0,0), None, pygame.BLEND_RGB_MULT)
+			boardsurf = orn
+
+		# Rotate and display board
+		rotsurf = pygame.transform.rotate(boardsurf, self.boardangle)
+		rotrect = rotsurf.get_rect()
+		rotrect.center = boardrect.center
+
+		self.display.blit(rotsurf, rotrect)
+
+		# Display player status
+		for i in range(len(self.players)):
+			string = "Player {}: {}".format(i + 1, 'Alive' if self.players[i].alive else 'Dead ')
+			img = self.font.render(string, 1, self.players[i].color)
+			pos = (i * (self.font.size(string)[0] + 10) + GRID_OFFSET_X, 0)
+			self.display.blit(img, pos)
+
+		# Display FPS
+		fps = self.font.render("FPS: {}".format(round(self.clock.get_fps(), 2)), 1, (255,255,255))
+		self.display.blit(fps, (0,0))
+
+		# Display rotation status
+		rotatestr = 'Rotating!'
+		if not self.rotating:
+			rottime = int(self.rotationtimer / 1000)
+			rotatestr = 'Next rotation: {}'.format(rottime)
+			
+		rot = self.font.render(rotatestr, 1, (255,255,255))
+		self.display.blit(rot, (0,15))
+
+		# Display flip status
+		flipstr = 'Inputs flipped!'
+		if not self.flipped:
+			fliptime = int(self.fliptimer / 1000)
+			flipstr = 'Next flip: {}'.format(fliptime)
+
+		flip = self.font.render(flipstr, 1, (255,255,255))
+		self.display.blit(flip, (0,30))
+
+		# Display render pause status
+		pausestr = 'Rendering stopped'
+		if not self.renderblanked:
+			pausetime = int(self.blanktimer / 1000)
+			pausestr = 'Next pause: {}'.format(pausetime)
+
+		ps = self.font.render(pausestr, 1, (255,255,255))
+		self.display.blit(ps, (0,45))
+
 		pygame.display.update()
 
-	def on_cleanup(self):
+	# Close everything
+	def cleanup(self):
 		pygame.display.quit()
 		pygame.quit()
 		sys.exit()
 
-	def on_execute(self):
-		self.on_init()
-
-		while self.running:
-			# Get pygame events
-			pygame.event.pump()
-			keys = pygame.key.get_pressed()
-
-			if GPIO.input(KLEFT):
-				if not self.lpressed:
-					self.lpressed = True
-
-					if self.player.direction == Player.UP:
-						self.player.moveLeft()
-					elif self.player.direction == Player.DOWN:
-						self.player.moveRight()
-					elif self.player.direction == Player.LEFT:
-						self.player.moveDown()
-					elif self.player.direction == Player.RIGHT:
-						self.player.moveUp()
-			else:
-				self.lpressed = False
-
-			if GPIO.input(KRIGHT):
-				if not self.rpressed:
-					self.rpressed = True
-
-					if self.player.direction == Player.UP:
-						self.player.moveRight()
-					elif self.player.direction == Player.DOWN:
-						self.player.moveLeft()
-					elif self.player.direction == Player.LEFT:
-						self.player.moveUp()
-					elif self.player.direction == Player.RIGHT:
-						self.player.moveDown()
-			else:
-				self.rpressed = False		
-
-			if keys[K_LEFT]:
-				self.player.moveLeft()
-			elif keys[K_RIGHT]:
-				self.player.moveRight()
-			elif keys[K_UP]:
-				self.player.moveUp()
-			elif keys[K_DOWN]:
-				self.player.moveDown()
-			if keys[K_ESCAPE]:
-				self.running = False
-
-			self.on_loop()
-			self.on_render()
-
-		self.on_cleanup()
-
 # Entrypoint
 if __name__ == '__main__':
-	# Setup GPIO stuff
+	args = sys.argv
+	print args
+
 	GPIO.setmode(GPIO.BCM)
+	
+	for pin in PINS:
+		GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
 
-	GPIO.setup(KLEFT, GPIO.IN, GPIO.PUD_DOWN)
-	GPIO.setup(KRIGHT, GPIO.IN, GPIO.PUD_DOWN)
+	pygame.init()
 
-	# Start game
 	game = Game()
-	game.on_execute()
+	game.start()
